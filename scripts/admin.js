5d0b257337e2d35d9e034ea515c18f0757d02342
Pallavi_270 <veeramanikantapallavi@gmail.com>
2024-12-28 16:45:04 +0530

    admin2.0

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,24 +1,63 @@
-const email= localStorage.getItem("email");
-if(!email){
+const email = localStorage.getItem("email");
+if (!email) {
     alert("Not logged in redirecting to homepage")
-window.location.href="home.html"
+    window.location.href = "home.html"
 }
-const form=document.getElementById("bookform");
-const bookgrid=document.getElementById("book-grid")
-document.addEventListener("DOMContentLoaded",tetchbooks);
+const form = document.getElementById("bookform");
+const bookgrid = document.getElementById("book-grid")
+document.addEventListener("DOMContentLoaded", tetchbooks);
 // add new book
-form.addEventListener("submit",(e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let title=form.title.value;
-    let author=form.author.value;
-    let category=form.category.value;
-    const book={
+    let title = form.title.value;
+    let author = form.author.value;
+    let category = form.category.value;
+    const book = {
         title,
         author,
         category,
-        isVerified:false,
-        borrowedDays:null;
-        availability:"Available"
+        isVerified: false,
+        borrowedDays: null;
+        availability: "Available"
     }
-    fetch()
+    fetch("http://localhost:3000/books", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+    })
+        .then((response) => response.json())
+        .then(() => {
+            alert("Book Added Successfully")
+            form.request();
+            fetchBooks();
+        })
+        .catch((err) => console.error("error adding book:", err));
+});
+// fetch books
+function fetchBooks(){
+
+}
+// create a book card
+function createBookCard(){
+    
+}
+// verify book
+const verifyButton=Clipboard.queryselector("verify");
+if(confirm("Are you sure toverify..?")){
+fetch("http://localhost:3000/books", {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({isVerified:true}),
 })
+    .then((response) => response.json())
+    .then(() => {
+        alert("Book Verified Successfully")
+        fetchBooks();
+    })
+    .catch((err) => console.error("error Verifying book:", err));
+}
+});
--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,11 @@ form.addEventListener("submit",function(event){
     let password=form.password.value;
     console.log(email,password)
     if(email=="admin@empher.com"&&password=="empher@123"){
+        localStorage.setItem("logindata",JSON.stringify({role:"admin",email}))
         alert("Logged in as Admin.")
         window.location.href = "admin.html"
     }else if(email=="user@empher.com"&&password=="user@123"){
+        localStorage.setItem("logindata",JSON.stringify({role:"user",email}))
         alert("rediecting to bookspage")
         window.location.href = "books.html"
     }else{
